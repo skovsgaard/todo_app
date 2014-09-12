3c25320167c12c62478ad994468d8cb4caedac1a
skovsgaard <bastian.1343@gmail.com>
2014-09-12 23:09:46 +0200

    CRUD working

--- web/controllers/entry_controller.ex
+++ web/controllers/entry_controller.ex
@@ -1,9 +1,12 @@
 defmodule TodoApp.EntryController do
   use Phoenix.Controller
   alias TodoApp.Router
+  alias TodoApp.Queries
+  alias TodoApp.Repo
+  alias TodoApp.Entry
 
   def index(conn, _params) do
-    entries = TodoApp.Queries.get_entries
+    entries = Queries.get_entries
     render conn, "index", entries: entries
   end
 
@@ -11,9 +14,34 @@ defmodule TodoApp.EntryController do
     render conn, "new"
   end
 
+  def show(conn, params) do
+    entry = Queries.get_by_id(params["id"])
+    render conn, "entry", entry: entry
+  end
+
+  def edit(conn, params) do
+    entry = Queries.get_by_id(params["id"])
+    render conn, "edit", entry: entry
+  end
+
   def create(conn, params) do
-    entry = %TodoApp.Entry{title: params["title"], due_date: params["due_date"], content: params["content"]} 
-    TodoApp.Repo.insert(entry)
+    entry = %Entry{title: params["title"], due_date: params["due_date"], content: params["content"]} 
+    Repo.insert(entry)
+    redirect conn, Router.entry_path(:index)
+  end
+
+  def update(conn, params) do
+    entry = Queries.get_by_id(params["id"])
+    entry = %{entry | title: params["title"],
+      due_date: params["due_date"],
+      content: params["content"]}
+    Repo.update(entry)
+    redirect conn, Router.entry_path(:index)
+  end
+
+  def destroy(conn, params) do
+    entry = Queries.get_by_id(params["id"])
+    Repo.delete(entry)
     redirect conn, Router.entry_path(:index)
   end
 end
--- web/models/queries.ex
+++ web/models/queries.ex
@@ -1,10 +1,19 @@
 defmodule TodoApp.Queries do
   import Ecto.Query
   alias TodoApp.Repo
+  alias TodoApp.Entry
 
   def get_entries do
-    query = from entry in TodoApp.Entry,
+    query = from entry in Entry,
             select: entry
     Repo.all(query)
+  end
+
+  def get_by_id(id) do
+    num_id = String.to_integer(id)
+    query = from entry in Entry,
+            where: entry.id == ^num_id,
+            select: entry
+    Repo.all(query) |> List.first
   end
 end
